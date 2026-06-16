808cf8b893936b6f90a8f649a39c70ef9faebca8
shobi-alam786 <shobialam87855@gmail.com>
2026-06-16 15:46:56 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const TodoHomePage(),
+    );
+  }
+}
+
+class Todo {
+  String id;
+  String title;
+  bool isDone;
+
+  Todo({required this.id, required this.title, this.isDone = false});
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'isDone': isDone,
+      };
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+        id: json['id'],
+        title: json['title'],
+        isDone: json['isDone'],
+      );
+}
+
+class TodoHomePage extends StatefulWidget {
+  const TodoHomePage({super.key});
+
+  @override
+  State<TodoHomePage> createState() => _TodoHomePageState();
+}
+
+class _TodoHomePageState extends State<TodoHomePage> {
+  List<Todo> _todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos();
+  }
+
+  Future<void> _loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? data = prefs.getString('todos');
+    if (data != null) {
+      final List decoded = jsonDecode(data);
+      setState(() {
+        _todos = decoded.map((e) => Todo.fromJson(e)).toList();
+      });
+    }
+  }
+
+  Future<void> _saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(
+        'todos', jsonEncode(_todos.map((e) => e.toJson()).toList()));
+  }
+
+  void _addTodo() {
+    final text = _controller.text.trim();
+    if (text.isEmpty) return;
+    setState(() {
+      _todos.add(Todo(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        title: text,
+      ));
+    });
+    _controller.clear();
+    _saveTodos();
+  }
+
+  void _toggleTodo(String id) {
+    setState(() {
+      final todo = _todos.firstWhere((t) => t.id == id);
+      todo.isDone = !todo.isDone;
+    });
+    _saveTodos();
+  }
+
+  void _deleteTodo(String id) {
+    setState(() {
+      _todos.removeWhere((t) => t.id == id);
+    });
+    _saveTodos();
+  }
+
+  int get _completedCount => _todos.where((t) => t.isDone).length;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('My Todo List'),
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(30),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Text(
+              '$_completedCount of ${_todos.length} completed',
+              style: const TextStyle(color: Colors.white70, fontSize: 13),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          // Input area
+          Container(
+            color: Colors.teal,
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    onSubmitted: (_) => _addTodo(),
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Add a new task...',
+                      hintStyle: const TextStyle(color: Colors.white54),
+                      filled: true,
+                      fillColor: Colors.teal[700],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: _addTodo,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.teal,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 14),
+                  ),
+                  child: const Text('Add'),
+                ),
+              ],
+            ),
+          ),
+
+          // Todo list
+          Expanded(
+            child: _todos.isEmpty
+                ? const Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.check_circle_outline,
+                            size: 64, color: Colors.teal),
+                        SizedBox(height: 16),
+                        Text('No tasks yet!',
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.grey)),
+                        SizedBox(height: 8),
+                        Text('Add something above to get started.',
+                            style: TextStyle(color: Colors.grey)),
+                      ],
+                    ),
+                  )
+                : ListView.builder(
+                    padding: const EdgeInsets.all(12),
+                    itemCount: _todos.length,
+                    itemBuilder: (context, index) {
+                      final todo = _todos[index];
+                      return Card(
+                        margin: const EdgeInsets.only(bottom: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: ListTile(
+                          leading: Checkbox(
+                            value: todo.isDone,
+                            activeColor: Colors.teal,
+                            onChanged: (_) => _toggleTodo(todo.id),
+                          ),
+                          title: Text(
+                            todo.title,
+                            style: TextStyle(
+                              decoration: todo.isDone
+                                  ? TextDecoration.lineThrough
+                                  : null,
+                              color:
+                                  todo.isDone ? Colors.grey : Colors.black87,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon:
+                                const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () => _deleteTodo(todo.id),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
